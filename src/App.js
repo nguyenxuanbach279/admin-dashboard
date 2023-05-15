a87d15df54929d2e78fbc7fd1aed289c93aef791
bachnx@bglobalcorp.com <nguyenxuanbach270901@gmail.com>
2023-05-15 11:14:35 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline } from "@mui/material";
+import "./App.css";
+import { ThemeProvider } from "@emotion/react";
+import Topbar from "./scenes/global/Topbar";
 
 function App() {
+  const [theme, colorMode] = useMode();
+
+  // console.log("theme", theme)
+  // console.log("colorMode", colorMode)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+          <CssBaseline />
+        <div className="app">
+          <main className="content">
+            <Topbar />
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
